50736d71f0b36c99c421fe5aaa716cce5b20b839
Renan Araujo <rlima@integranaweb.com.br>
2022-02-24 17:13:59 -0300

    introdução

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
-alert("Meu primeiro JS");
+var nome = "Renan Araujo";
+var idade = 26;
+
+alert(
+  `Bem vindo ${nome.replace("Renan Araujo", "João")} que tem ${idade} anos`
+);
+
+var frase = "O Japão é o melhor time do mundo";
+
+console.log(frase.replace("Japão", "Brasil"));
